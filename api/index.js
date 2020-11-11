467a5a27f47d628873b5a67b9d09f4bc87b9a77c
AmiralBl3ndic <camille.briand@efrei.net>
2020-11-11 17:14:15 +0100

    feat: plug SwaggerUI at /api/docs path

--- api/index.js
+++ api/index.js
@@ -1,9 +1,14 @@
 'use strict';
 
 const express = require('express');
+const swaggerUI = require('swagger-ui-express');
 const { StatusCodes, getReasonPhrase } = require('http-status-codes');
 
+const openAPISpecs = require('yamljs').load('./api/openapi.yml');
+
 const app = express();
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(openAPISpecs));
 
 app.use((req, res, next) => {
 	res.status(StatusCodes.NOT_FOUND).json({
