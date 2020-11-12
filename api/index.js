6b65581b1f71b6296a2c46ae3a595576ae596216
AmiralBl3ndic <camille.briand@efrei.net>
2020-11-12 23:51:10 +0100

    feat(passport): initialize passport for app in api/index.js

--- api/index.js
+++ api/index.js
@@ -3,14 +3,16 @@
 const express = require('express');
 const swaggerUI = require('swagger-ui-express');
 const { StatusCodes, getReasonPhrase } = require('http-status-codes');
-
 const openAPISpecs = require('yamljs').load('./api/openapi.yml');
+
+const configuredPassport = require('./auth/passport.config');
 const routes = require('./routes/root.route');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(configuredPassport.initialize());
 
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(openAPISpecs));
 app.use('/', routes);
