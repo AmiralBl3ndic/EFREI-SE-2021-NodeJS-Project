ad1c300ffb9a6a1b40b0447994bacd1e8cb6f0db
AmiralBl3ndic <camille.briand@efrei.net>
2020-11-12 23:18:57 +0100

    feat(LocalStrategy): add custom passportjs LocalStrategy

---
+++ api/auth/local.strategy.js
@@ -0,0 +1,19 @@
+const LocalStrategy = require('passport-local').Strategy;
+const argon2 = require('argon2');
+
+const UserService = require('../services/user.service');
+
+module.exports = new LocalStrategy(async function (username, password, done) {
+	try {
+		const user = await UserService.findByUsername(username);
+
+		if (!user) return done(null, false, { message: 'User not found' });
+
+		if (await argon2.verify(user.password, password)) {
+			return done(null, user);
+		}
+		return done(null, false, { message: 'Wrong password' });
+	} catch (error) {
+		return done(error);
+	}
+});
