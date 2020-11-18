1e6d208af4da2aa9c623768b6505866dfe09125a
AmiralBl3ndic <camille.briand@efrei.net>
2020-11-18 13:17:33 +0100

    feat: add title to index page
    This is done using Next's Head component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import { NextPage } from 'next';
+import Head from 'next/head';
 
 const Index: NextPage = () => {
 	return (
 		<>
+			<Head>
+				<title>Home page</title>
+			</Head>
+
 			<h1>Hello world</h1>
 			<p>This is a notes taking app</p>
 		</>
