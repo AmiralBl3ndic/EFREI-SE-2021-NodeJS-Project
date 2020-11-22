555299e1a7166666aea077e0621c96df0ce599b2
jules <jules.lagny@efrei.net>
2020-11-22 15:47:15 +0100

    define Welcome pages architecture

---
+++ components/WelcomeMessage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WelcomeMessage = () => {
+	return <>WelcomeMessage</>;
+}
+
+export default WelcomeMessage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,12 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import Welcome from './Welcome';
 
 const Index: NextPage = () => {
 	return (
 		<>
-			<Head>
-				<title>Home page</title>
-			</Head>
-
-			<>
-				<Button variant="peimary">Primary</Button>{' '}
-				<Button variant="secondary">Secondary</Button>{' '}
-				<Button variant="success">Success</Button>{' '}
-				<Button variant="warning">Warning</Button>{' '}
-				<Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
-				<Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
-				<Button variant="link">Link</Button>
-			</>
-
-			<h1>Hello world</h1>
-			<p>This is a notes taking app</p>
+			<Welcome/>
 		</>
 	);
 };
