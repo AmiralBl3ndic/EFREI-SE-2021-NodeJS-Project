a5df9b4c3c35b92dd9cc057836eabfe7121a3dee
jules <jules.lagny@efrei.net>
2020-11-24 17:45:48 +0100

    login form design done

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,9 +1,76 @@
-import React from "react";
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const LoginForm = () => {
+	const { register, handleSubmit, watch, errors } = useForm();
+	const onSubmit = (data) => console.log(data);
+
+	console.log(watch('example')); // watch input value by passing the name of it
+
 	return (
-		<div>yo</div>
+		<div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+			<div className="max-w-md w-full space-y-8">
+				<div>
+					<img
+						className="mx-auto h-20 w-auto"
+						src="http://placedelacomedie.com/wp-content/uploads/2017/07/Ricard_logo.png"
+						alt="Workflow"
+					/>
+					<br />
+					<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+						Sign in to your account
+					</h2>
+				</div>
+
+				<br />
+				<form className="mt-8 space-y-6" onSubmit={handleSubmit(onSubmit)}>
+					<input type="hidden" name="remember" value="true" />
+					<div className="rounded-md shadow-sm -space-y-px">
+						<div>
+							<label htmlFor="email-address" className="sr-only">
+								Email address
+							</label>
+							<input
+								id="email-address"
+								name="email"
+								type="email"
+								required
+								className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+								placeholder="Email address"
+								ref={register({ required: true })}
+							/>
+						</div>
+						<div>
+							<label htmlFor="password" className="sr-only">
+								Password
+							</label>
+							<input
+								id="password"
+								name="password"
+								type="password"
+								required
+								className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+								placeholder="Password"
+								ref={register({ required: true })}
+							/>
+						</div>
+					</div>
+
+					{errors.exampleRequired && <span>This field is required</span>}
+
+					<br />
+					<div>
+						<button
+							type="submit"
+							className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+						>
+							Sign in
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
 	);
-}
+};
 
 export default LoginForm;
