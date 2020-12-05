a0631f91c8aa4e9f0794b190adcc4122c91e3fcb
AmiralBl3ndic <camille.briand@efrei.net>
2020-12-05 16:18:48 +0100

    feat(NJP-2): make canReadNote middleware use request object for parameters

--- api/middlewares/auth/can-read-note.middleware.js
+++ api/middlewares/auth/can-read-note.middleware.js
@@ -5,11 +5,11 @@ const supabase = require('../../db');
  * Checks whether the currently authenticated user can read a specific note
  * @param {uuid} noteId ID of the note to check reading rights of authenticated user
  */
-const canReadNote = (noteId) => async (req, res, next) => {
+const canReadNote = async (req, res, next) => {
 	// Check if user is authenticated, reject if not
 	if (!req.user)
 		return res.status(StatusCodes.UNAUTHORIZED).json({
-			resource: noteId,
+			resource: req.params.noteId,
 		});
 
 	// Retrieve note and access rights for user from database
@@ -17,7 +17,7 @@ const canReadNote = (noteId) => async (req, res, next) => {
 		.from('user_notes_with_rights')
 		.select('can_read')
 		.eq('username', req.user.username)
-		.eq('note_id', noteId)
+		.eq('note_id', req.params.noteId)
 		.limit(1);
 
 	if (error) return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
@@ -25,7 +25,7 @@ const canReadNote = (noteId) => async (req, res, next) => {
 	// If no results or if no reading rights
 	if (data.length === 0 || !data[0].can_read)
 		return res.status(StatusCodes.FORBIDDEN).json({
-			resource: noteId,
+			resource: req.params.noteId,
 		});
 
 	next();
