bef53eb60ea06d4ef4ebcf1dac5d9d9af70fc027
AmiralBl3ndic <camille.briand@efrei.net>
2020-12-05 12:20:06 +0100

    chore: continue package manager updates

--- api/routes/auth/index.js
+++ api/routes/auth/index.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
+const passport = require('../../');
 
 const registerRoutes = require('./register.routes');
 
 router.use('/register', registerRoutes);
+router.use('/login', passport.authenticate('local'));
 
 module.exports = router;
