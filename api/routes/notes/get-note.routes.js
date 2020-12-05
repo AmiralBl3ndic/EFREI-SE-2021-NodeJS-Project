161eb60e3c837b32c239d2b2bc3cd037dfaa5b24
AmiralBl3ndic <camille.briand@efrei.net>
2020-12-05 16:19:38 +0100

    feat(api): add GET /notes/:noteId endpoint

---
+++ api/routes/notes/get-note.routes.js
@@ -0,0 +1,19 @@
+const { StatusCodes } = require('http-status-codes');
+const canReadNote = require('../../middlewares/auth/can-read-note.middleware');
+const supabase = require('../../db');
+
+const router = require('express').Router();
+
+router.get('/:noteId', canReadNote, async (req, res) => {
+	const { data, error } = await supabase
+		.from('notes')
+		.select('note_id,title,current_content')
+		.eq('note_id', req.params.noteId)
+		.limit(1);
+
+	if (error) return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
+
+	return res.status(StatusCodes.OK).json(data[0]);
+});
+
+module.exports = router;
