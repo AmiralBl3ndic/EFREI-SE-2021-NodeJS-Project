caddfa107934a79c343b2a0d26c14aabb1fbcd18
jules <jules.lagny@efrei.net>
2020-12-09 14:40:29 +0100

    edit: je me souviens plus

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import { Alert } from '@windmill/react-ui';
+import RegisterForm from 'components/RegisterForm';
+import Welcome from 'components/Welcome';
 import { NextPage } from 'next';
 import React from 'react';
 import LoginForm from '../components/LoginForm';
@@ -6,7 +8,9 @@ import LoginForm from '../components/LoginForm';
 const Index: NextPage = () => {
 	return (
 		<>
-			<div className="container mx-auto">
+			<div className="position-fixed w-100 h-100 left-0 right-0">
+				<Welcome />
+				<RegisterForm />
 				<LoginForm />
 			</div>
 		</>
@@ -14,22 +18,3 @@ const Index: NextPage = () => {
 };
 
 export default Index;
-
-/*
-
-<div className="grid">
-				<div className="welcomeArea">
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ad
-						optio repellendus ab tenetur ratione ipsam libero illum dicta saepe
-						odit nisi eligendi delectus dignissimos placeat dolorem porro,
-						voluptatem nostrum.
-					</p>
-				</div>
-				<div className="loginForm">
-					<LoginForm />
-					<Alert type="warning">Lorem ipsum dolor sit</Alert>
-				</div>
-			</div>
-
-			*/
