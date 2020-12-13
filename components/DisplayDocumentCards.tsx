7e95fea125acc99ecab4ed1b36a7c65704c9e34d
jules <jules.lagny@efrei.net>
2020-12-13 22:44:31 +0100

    edit: make list of cards + simulation of fetch

--- components/DisplayDocumentCards.tsx
+++ components/DisplayDocumentCards.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import DocumentCard from './DocumentCard';
 
+interface documentProps {
+	title: string;
+}
+
+const resources = [
+	{ title: 'Les joueurs du PSG' },
+	{ title: 'Tuchel : est-il si mauvais?' },
+	{ title: 'Un ricard et ça repart !' },
+	{ title: "8h et demi ? l'heure du demi" },
+	{ title: 'pitiez finissons ce semestre' },
+];
+
 const DisplayDocumentCards = () => {
+	const [documents, setDocuments] = React.useState<documentProps[]>([]);
+
+	useEffect(() => {
+		setDocuments(resources);
+	});
+
 	return (
 		<>
-			<DocumentCard id={1} title="epsum moriculus mbappus neymarus" />
-			<DocumentCard id={1} title="epsum moriculus mbappus neymarus" />
-			<DocumentCard id={1} title="epsum moriculus mbappus neymarus" />
-			<DocumentCard id={1} title="epsum moriculus mbappus neymarus" />
-			<DocumentCard id={1} title="epsum moriculus mbappus neymarus" />
-			<DocumentCard id={1} title="epsum moriculus mbappus neymarus" />
+			{documents.map((doc, index) => {
+				return <DocumentCard id={index + 1} title={doc.title} />;
+			})}
 		</>
 	);
 };
