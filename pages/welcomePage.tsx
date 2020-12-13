e8f0df650505c9976610af57b8992eba1ae71cd4
jules <jules.lagny@efrei.net>
2020-12-13 22:46:04 +0100

    edit: do a bit of clean on code

--- pages/welcomePage.tsx
+++ pages/welcomePage.tsx
@@ -1,14 +1,10 @@
 import LoginForm from 'components/LoginForm';
-import NavBar from 'components/NavBar';
 import RegisterForm from 'components/RegisterForm';
 import WelcomeMessage from 'components/WelcomeMessage';
 import { NextPage } from 'next';
 import React from 'react';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container';
 
 const WelcomePage: NextPage = () => {
 	return (
