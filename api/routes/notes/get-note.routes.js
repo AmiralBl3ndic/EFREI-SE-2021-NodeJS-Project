3489ae8438454c10c045320864a771afc3966784
Gaullier <gaullier.theo@gmail.com>
2020-12-13 21:48:37 +0100

    feat(get-note.route): Revision created and stored

--- api/routes/notes/get-note.routes.js
+++ api/routes/notes/get-note.routes.js
@@ -5,7 +5,6 @@ const canCreateNote = require('../../middlewares/auth/can-create-note.middleware
 const isAuthor = require('../../middlewares/auth/is-author.middleware');
 const NotesService = require('../../services/notes.service');
 const supabase = require('../../db');
-
 const router = require('express').Router();
 
 // Get note informations
@@ -19,7 +18,7 @@ router.get('/users/:username/notes/:noteId', canReadNote, async (req, res) => {
 	if (error) throw new Error(error.message);
 
 	return res.status(StatusCodes.OK).json({
-		username: req.params.username,
+		author: req.params.username,
 		title: data[0].title,
 		content: data[0].current_content,
 		id: data[0].note_id,
@@ -34,7 +33,7 @@ router.post('/users/:username/notes', canCreateNote, async (req, res) => {
 			title: req.body.title,
 		},
 	]);
-	const { data2, error2 } = await supabase.from('notes_access_control').insert([
+	await supabase.from('notes_access_control').insert([
 		{
 			user_id: req.user.user_id,
 			note: data[0].note_id,
@@ -60,7 +59,6 @@ router.patch('/users/:username/notes/:noteId', isAuthor, async (req, res) => {
 		.from('notes')
 		.update({ title: req.body.title })
 		.match({ note_id: req.params.noteId });
-	//.match({ author: req.user.id })
 
 	if (error) throw new Error(error.message);
 
@@ -73,7 +71,11 @@ router.delete('/users/:username/notes/:noteId', isAuthor, async (req, res) => {
 		.from('notes')
 		.delete()
 		.match({ note_id: req.params.noteId });
-	//.match({ author: req.user.id })
+
+	await supabase
+		.from('notes_access_controle')
+		.delete()
+		.match({ note_id: req.params.noteId });
 
 	if (error) throw new Error(error.message);
 
@@ -82,36 +84,28 @@ router.delete('/users/:username/notes/:noteId', isAuthor, async (req, res) => {
 
 // Revision created and stored
 router.post(
+	// TODO
 	'/users/:username/notes/:noteId/revisions',
-	isAuthor,
+	canWriteNote,
 	async (req, res) => {
-		const revision = await supabasefrom('revisions').insert([
-			{
-				createdat: Date.now(),
-				note: req.params.noteId,
-			},
-		]);
+		const contentBefore = await supabase
+			.from('notes')
+			.select('current_content')
+			.eq('note_id', req.params.noteId);
 
-		const new_content = await NotesService.buildNoteContentFromRevisions(
-			revision,
+		const revision = await NotesService.createRevisionFromContentDifference(
+			contentBefore,
+			contentAfter,
 		);
-
-		const { data2, error2 } = await supabasefrom('modifications').insert([
-			{
-				createdat: Date.now(),
-				note: req.params.noteId,
-			},
-		]);
 
 		if (error) throw new Error(error.message);
 
 		return res.status(StatusCodes.CREATED).json({
-			id: data[0].note_id,
-			link: '/api/users/'
-				.concat(req.params.username)
-				.concat('/notes/')
-				.concat(data[0].note_id),
-			title: req.body.title,
+			hash: revision.revision_id,
+			timestap: revision.createdat,
+			username: req.params.username,
+			changes: req.body.changes,
+			message: req.body.message,
 		});
 	},
 );
