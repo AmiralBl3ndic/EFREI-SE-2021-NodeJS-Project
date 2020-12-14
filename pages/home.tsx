4b5b37a6b9ff2b5f7f6320140aae238ac4333bae
AmiralBl3ndic <camille.briand@efrei.net>
2020-12-14 16:04:14 +0100

    feat(pages/Home): add "navigation" ARIA role to left column

--- pages/home.tsx
+++ pages/home.tsx
@@ -5,7 +5,10 @@ import React from 'react';
 const Home: NextPage = () => {
 	return (
 		<div className="row vh-100">
-			<div className="col-12 col-md-3 d-flex flex-column bg-primary">
+			<div
+				className="col-12 col-md-3 d-flex flex-column bg-primary"
+				role="navigation"
+			>
 				<DisplayDocumentCards />
 			</div>
 			<div className="col-12 col-md-6 d-flex flex-column bg-secondary border">
