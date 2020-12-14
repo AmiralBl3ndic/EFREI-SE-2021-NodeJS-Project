57fb6ff4d5b382c638b39968c030d12c4f533cdc
jules <jules.lagny@efrei.net>
2020-12-14 21:30:52 +0100

    edit: login placeholder

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -49,7 +49,7 @@ const LoginForm = () => {
 							<input
 								name="username"
 								className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-								placeholder="Email address"
+								placeholder="Username"
 								ref={register({ required: true })}
 							/>
 						</div>
